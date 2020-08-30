9d1c5e1a07ebe0896b7eea73e8c6e8952147e157
mridarora <aroram15@mcmaster.ca>
2020-08-30 20:43:48 +0530

    final touches done

--- js/resp.js
+++ js/resp.js
@@ -2,19 +2,9 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
 rightNav = document.querySelector('.rightNav')
-jml = document.querySelector('.hide')
 
 burger.addEventListener('click', () =>{
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
 })
-
-function myFunction() {
-    var x = document.getElementsByClassName("hide");
-    if (x.style.display == "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
